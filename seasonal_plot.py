8f2a4eb9865c5a7a14f99c9d41eab5a3383f1b81
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2021-12-04 11:50:00 +0300

    Create seasonal_plot.py

---
+++ seasonal_plot.py
@@ -0,0 +1,29 @@
+def seasonal_plot(X, y, period, freq, ax=None):
+    if ax is None:
+        _, ax = plt.subplots()
+    palette = sns.color_palette("husl", n_colors=X[period].nunique(),)
+    ax = sns.lineplot(
+        x=freq,
+        y=y,
+        hue=period,
+        data=X,
+        ci=False,
+        ax=ax,
+        palette=palette,
+        legend=False,
+    )
+    ax.set_title(f"Seasonal Plot ({period}/{freq})")
+    for line, name in zip(ax.lines, X[period].unique()):
+        y_ = line.get_ydata()[-1]
+        ax.annotate(
+            name,
+            xy=(1, y_),
+            xytext=(6, 0),
+            color=line.get_color(),
+            xycoords=ax.get_yaxis_transform(),
+            textcoords="offset points",
+            size=14,
+            va="center",
+        )
+    return ax
+        
